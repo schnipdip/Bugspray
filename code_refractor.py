52aefaf681bbb5661d10cf06701c2b2049ed17d8
schnipdip <cjh30@pct.edu>
2020-07-22 09:13:41 -0400

    Add files via upload

--- code_refractor.py
+++ code_refractor.py
@@ -1,3 +1,4 @@
+import argparse
 import yaml
 import sys
 
@@ -8,12 +9,15 @@ def yaml_load(path):
     return data
 
 
-def parse_data(data):
+def parse_data(data, exclusion):
     new = []
     blank = []
     counter = 0
-
+    
     for i in data:
+        if exclusion in i.values():
+            print (i)
+            new.append(i)
         if 'debug' not in i.keys() and 'block' not in i.keys():
             '''Locates the Debug: tags'''
             new.append(i)
@@ -34,15 +38,25 @@ def generate_yaml(path, write_data):
 
 if __name__ == "__main__":
 
-    path = sys.argv[1]
+    #path = sys.argv[1]
+    #path = "C:\\Users\\Chris Herzog\\Documents\\Python Scripts\Ansible Code Refactor\\testcode.yml"
 
+    parser = argparse.ArgumentParser(description='This program will remove all debug or exclude a keyword debug')
+    
+    parser.add_argument('-i', action='store', dest='path', help='Stores the path to the yaml code')
+    parser.add_argument('-e', action='store', dest='exclusion', help='Stores the keyword to exclude from deletion')
+
+    result = parser.parse_args()
+                        
+    path = result.path
+    exclusion = result.exclusion
+                        
     if (path.endswith('.yml') or path.endswith('.yaml')):
         data = yaml_load(path)
-        write_data = parse_data(data)
+        write_data = parse_data(data, exclusion)
         new_file = generate_yaml(path, write_data)
 
         print ('Successfully Refactored', path)
     else:
         print ('Invalid extension type')
         exit(0)
-
